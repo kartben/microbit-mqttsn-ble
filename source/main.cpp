6d3592bdd8aadd68d1172676a6c6e2e85c6f6527
Benjamin Cabé <benjamin@eclipse.org>
2016-12-19 16:12:13 +0100

    transmit BLE packets in chunks of 18bytes (Nordic bug?)
    Signed-off-by: Benjamin Cabé <benjamin@eclipse.org>

--- source/main.cpp
+++ source/main.cpp
@@ -23,32 +23,104 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-// Works with the Animal Vegetable Mineral guessing game in the Android 'micro:bit Blue' app which is obtainable from
-// https://play.google.com/store/apps/details?id=com.bluetooth.mwoolley.microbitbledemo
-
 #include "MicroBit.h"
+#include "MicroBitSerial.h"
 #include "MicroBitUARTService.h"
+
+#include "MQTTSNPacket.h"
 
 MicroBit uBit;
 MicroBitUARTService *uart;
+MicroBitSerial serial(USBTX, USBRX); 
 
 int connected = 0;
 
+int rc = 0;
+unsigned char buf[200];
+int buflen = sizeof(buf);
+MQTTSN_topicid topic;
+int len = 0;
+int dup = 0;
+int qos = 0;
+int retained = 0;
+short packetid = 0;
+//  char *topicname = "a long topic name";
+MQTTSNPacket_connectData options = MQTTSNPacket_connectData_initializer;
+
+#define BLE_CHUNK_SIZE 18
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
+int send_in_chunks(unsigned char *buffer, size_t length, size_t chunk_size)
+{
+    ssize_t n;
+    const unsigned char *p = buffer;
+    while (length > 0)
+    {
+        n = uart->send(p, min(length, chunk_size));
+        if (n <= 0) break;
+        p += n;
+        length -= n;
+    }
+    return (n <= 0) ? -1 : 0;
+}
+
+
+/** MQTT-SN transport helpers **/
+int transport_sendPacketBuffer(unsigned char* buf, int buflen)
+{
+    int rc = send_in_chunks(buf, buflen, BLE_CHUNK_SIZE);
+    return rc;
+}
+
+
+int transport_getdata(unsigned char* buf, int count)
+{
+    int rc = uart->read(buf, count, ASYNC);
+    return rc;
+}
+
+int transport_open()
+{
+    // Note GATT table size increased from default in MicroBitConfig.h
+    // #define MICROBIT_SD_GATT_TABLE_SIZE             0x500
+    uart = new MicroBitUARTService(*uBit.ble, 128, 128);
+
+    return 0;
+}
+
+int transport_close()
+{
+    return 0;
+}
+
+
 void onConnected(MicroBitEvent)
 {
-
     uBit.display.scroll("C");
 
     connected = 1;
 
-    // mobile app will send ASCII strings terminated with the colon character
-    ManagedString eom(":");
+    options.clientID.cstring = microbit_friendly_name();
+    len = MQTTSNSerialize_connect(buf, buflen, &options);
+    rc = transport_sendPacketBuffer(buf, len);
 
-    while(connected == 1) {
-        ManagedString msg = uart->readUntil(eom);
-        uBit.display.scroll(msg);
+    /* wait for connack */
+    if (MQTTSNPacket_read(buf, buflen, transport_getdata) == MQTTSN_CONNACK)
+    {
+        int connack_rc = -1;
+
+        if (MQTTSNDeserialize_connack(&connack_rc, buf, buflen) != 1 || connack_rc != 0)
+        {
+            // TODO goto exit;
+        }
+        else 
+            printf("connected rc %d\n", connack_rc);
+    } else {
+        // TODO exit
     }
-
 }
 
 void onDisconnected(MicroBitEvent)
@@ -57,13 +129,26 @@ void onDisconnected(MicroBitEvent)
     connected = 0;
 }
 
+// publishes an MQTT-SN message. 
+// keep in mind that BLE payloads over >19bytes might not work that well so try to 
+// stay concise!
+void publish(char * payload) {
+    /* publish with short name */
+    topic.type = MQTTSN_TOPIC_TYPE_SHORT;
+    memcpy(topic.data.short_name, "mb", 2);
+
+    len = MQTTSNSerialize_publish(buf, buflen, dup, qos, retained, packetid,
+            topic, (unsigned char *) payload, strlen(payload));
+    rc = transport_sendPacketBuffer(buf, len);
+}
+
+
 void onButtonA(MicroBitEvent)
 {
     if (connected == 0) {
         return;
     }
-    uart->send(ManagedString("YES"));
-    uBit.display.scroll("Y");
+    publish("A");
 }
 
 void onButtonB(MicroBitEvent)
@@ -71,8 +156,7 @@ void onButtonB(MicroBitEvent)
     if (connected == 0) {
         return;
     }
-    uart->send(ManagedString("NO"));
-    uBit.display.scroll("N");
+    publish("BBB");
 }
 
 void onButtonAB(MicroBitEvent)
@@ -80,8 +164,16 @@ void onButtonAB(MicroBitEvent)
     if (connected == 0) {
         return;
     }
-    uart->send(ManagedString("GOT IT!!"));
-    uBit.display.scroll("!");
+
+    char mypayload[250];
+    sprintf((char*)mypayload, 
+        "{\"x\":%d,\"y\":%d,\"z\":%d}",
+        uBit.accelerometer.getX(), 
+        uBit.accelerometer.getY(), 
+        uBit.accelerometer.getZ()
+    ); 
+
+    publish(mypayload);
 }
 
 int main()
@@ -95,11 +187,7 @@ int main()
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, onButtonAB);
 
-
-    // Note GATT table size increased from default in MicroBitConfig.h
-    // #define MICROBIT_SD_GATT_TABLE_SIZE             0x500
-    uart = new MicroBitUARTService(*uBit.ble, 32, 32);
-    uBit.display.scroll("UART ready");
+    transport_open();
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
